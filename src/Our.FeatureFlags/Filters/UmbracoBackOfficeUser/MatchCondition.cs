bc1ff343614c89ca0f3bda34eb13a8cfab4e7cf7
Matthew Wise <6782865+Matthew-Wise@users.noreply.github.com>
2022-08-02 23:21:15 +0100

    added new demo site and editorconfig

---
+++ src/Our.FeatureFlags/Filters/UmbracoBackOfficeUser/MatchCondition.cs
@@ -0,0 +1,29 @@
+﻿namespace Our.FeatureFlags.Filters.UmbracoBackOfficeUser;
+
+using Microsoft.FeatureManagement;
+
+public class MatchCondition
+{
+
+    /// <inheritdoc cref="RequirementType"/>
+    public RequirementType Match { get; set; } = RequirementType.Any;
+
+    public string[] Values { get; set; } = Array.Empty<string>();
+
+    public bool IsMatch(IEnumerable<string> valuesToMatch)
+    {
+        if(Values.Any() == false)
+        {
+            return true;
+        }
+
+        return Match switch
+        {
+            RequirementType.All => Values.All(v => valuesToMatch.Contains(v, StringComparer.InvariantCultureIgnoreCase)),
+            RequirementType.Any => Values.Any(v => valuesToMatch.Contains(v, StringComparer.InvariantCultureIgnoreCase)),
+            _ => false
+        };
+    }
+
+    public bool IsMatch(string email) => IsMatch(new[] { email });
+}
